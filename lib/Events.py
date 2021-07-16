4a6d09efc7edcdc29cfe5d773bbe0c64d83cd066
moi <moi@localhost>
2021-07-16 13:54:19 +0200

    Recherche d'une solution pour le clavier en console.

--- lib/Events.py
+++ lib/Events.py
@@ -1,2 +1,44 @@
 
+import sys
+import termios
+import contextlib
 
+
+class Events:
+    def __init__(self,events: dict,quit = 'Q'):
+        self.e_dict = events
+        self.key_exit = quit
+
+
+    @contextlib.contextmanager
+    def raw_mode(file):
+        old_attrs = termios.tcgetattr(file.fileno())
+        new_attrs = old_attrs[:]
+        new_attrs[3] = new_attrs[3] & ~(termios.ECHO | termios.ICANON)
+        try:
+            termios.tcsetattr(file.fileno(), termios.TCSADRAIN, new_attrs)
+            yield
+        finally:
+            termios.tcsetattr(file.fileno(), termios.TCSADRAIN, old_attrs)
+
+    def add_event(self):
+        return
+
+    def wait(self):
+        with raw_mode(sys.stdin):
+            try:
+                s=''
+                while True:
+                    ch = sys.stdin.read(1)
+                    if not ch or ch == chr(4):
+                        break
+                    if ch == "\x1b":
+                        continue 
+                        s += ch
+                    print('s:',s)
+
+
+    
+
+    def terminate(self):
+        sys.exit(0)
---
+++ lib/keyboards/azerty.py
@@ -0,0 +1,120 @@
+
+KEY_UP      =  1
+KEY_DOWN    =  2
+KEY_LEFT    =  3
+KEY_RIGHT   =  4
+KEY_PGUP    =  5
+KEY_PGDOWN  =  6
+KEY_START   =  7
+KEY_END     =  8
+KEY_INSERT  =  9
+KEY_DELETE  =  10
+KEY_ENTER   =  11
+KEY_BACK    =  12
+
+KEY_F1       = 13
+KEY_F2       = 14
+KEY_F3       = 15
+KEY_F4       = 16
+KEY_F5       = 17
+KEY_F6       = 18
+KEY_F7       = 19
+KEY_F8       = 20
+KEY_F9       = 21
+KEY_F10      = 22
+KEY_F11      = 23
+KEY_F12      = 24
+
+KEY_BACKSPACE = 25 
+KEY_TAB       = 26
+
+special_key_string = [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        'BEL',
+        'BS',
+        'TAB',
+        'LF',
+        'LF',
+        'LF',
+        'CR',
+        'SO',
+        'S1',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        'CAN',
+        '',
+        '',
+        'ESC',
+        ]
+
+
+function_key_strings = [
+        'KEY_UP',
+        'KEY_DOWN',
+        'KEY_LEFT',
+        'KEY_RIGHT',
+        'KEY_PGUP',
+        'KEY_PGDOWN',
+        'KEY_START',
+        'KEY_END',
+        'KEY_INSERT',
+        'KEY_DELETE',
+        'KEY_ENTER',
+        'KEY_BACK',
+        
+        'KEY_F1',
+        'KEY_F2',
+        'KEY_F3',
+        'KEY_F4',
+        'KEY_F5',
+        'KEY_F6',
+        'KEY_F7',
+        'KEY_F8',
+        'KEY_F9',
+        'KEY_F10',
+        'KEY_F11',
+        'KEY_F12',
+        
+        'KEY_BACKSPACE',
+        'KEY_TAB'
+]
+
+
+function_keys  = {
+        '[A'   :  KEY_UP ,
+        '[B'   :  KEY_DOWN, 
+        '[C'   :  KEY_LEFT,
+        '[D'   :  KEY_RIGHT,
+        '[5~'  : KEY_PGUP,
+        '[6~'  : KEY_PGDOWN,
+        '[H'   : KEY_START,
+        '[F'   : KEY_END,
+        '[2~'  : KEY_INSERT,
+        '[3~'  : KEY_DELETE,
+        'OP'   : KEY_F1,
+        'OQ'   : KEY_F2,
+        'OR'   : KEY_F3,
+        'OS'   : KEY_F4,
+        '[15~' : KEY_F5,
+        '[17~' : KEY_F6,
+        '[18~' : KEY_F7,
+        '[19~' : KEY_F8,
+        '[20~' : KEY_F9,
+        '[21~' : KEY_F10,
+        '[23~' : KEY_F11,
+        '[24~' : KEY_F12
+        }
+
+
+
